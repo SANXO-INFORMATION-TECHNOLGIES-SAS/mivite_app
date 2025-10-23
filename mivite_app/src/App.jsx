0c7b284cc9bd316486448dfe00695269170f40aa
Luis Fernando Sanchez <43077713+luisfsanchezp@users.noreply.github.com>
2025-10-23 15:35:53 +0000

    Creo la app desde node y agrego algunos archivos.

---
+++ mivite_app/src/App.jsx
@@ -0,0 +1,58 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      
+        <button onClick={() => Curriculum()}>Ver Curriculum
+        </button>
+    </>
+  )
+}
+
+const Curriculum =()=>{
+    return (
+    <div>
+            <p>
+                Hola!! 
+            </p>
+            <p>
+                Soy Luis Fernando Sanchez Perez
+            </p>
+            <hr>
+            <p>
+                <h1>WOW!!!!</h1>
+            </p>
+
+    </div>
+    );
+};
+
+module.export.Curriculum = Curriculum;
+
+export default App
